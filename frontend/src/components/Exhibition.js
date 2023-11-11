e6d8d23eedbe708a820844b17d5f778062caca45
park0jae <okvv26@naver.com>
2023-11-11 19:39:13 +0900

    refactor : 전시 목록 디자인 수정

--- frontend/src/components/Exhibition.js
+++ frontend/src/components/Exhibition.js
@@ -12,7 +12,6 @@ export function Exhibition(props) {
   
   const navigate = useNavigate();
   const handleAddBtnClicked = e => {
-    console.log(imageId);
     props.setOneExhibition(exhibitionName);
     props.setPrice(price);
   };
@@ -23,7 +22,6 @@ export function Exhibition(props) {
             responseType : 'arraybuffer',
         });
         const imageUrl = URL.createObjectURL(new Blob([response.data], {type: 'image/jpeg'}));
-        console.log(imageUrl);
         return imageUrl;
     }catch(error) {
         console.log(error);
@@ -38,7 +36,7 @@ export function Exhibition(props) {
   return <>
     <div style={{display: 'flex', alignItems: 'center', width: '100%'}}>
       <img src={imageUrl} />
-      <div style={{marginLeft: '5px', flexGrow: '1'}}>{exhibitionName}</div>
+      <div style={{marginLeft: '5px', flexGrow: '1', width:'80px'}}>{exhibitionName}</div>
       <div className="col text-center price">{price}원</div>
       <div className="col text-end action" style={{display:'flex', gap: '5px'}}>
         <button onClick={()=>{
@@ -48,7 +46,7 @@ export function Exhibition(props) {
           }
         });
       }} className="btn btn-small btn-outline-dark">상세보기</button>
-        <button onClick={handleAddBtnClicked} className="btn btn-small btn-outline-dark">예약하기</button>
+        <button onClick={handleAddBtnClicked} className="btn btn-small btn-outline-dark">예약</button>
       </div>
     </div>
   </>
